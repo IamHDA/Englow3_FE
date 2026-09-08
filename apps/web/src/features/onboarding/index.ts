e34749279f7d4b7e8e760df3000c0cd64571e066
IamHDA <huaduyanh2010@gmail.com>
2026-09-08 18:29:24 +0700

    refactor: code structure

--- apps/web/src/features/onboarding/index.ts
+++ apps/web/src/features/onboarding/index.ts
@@ -1,6 +1,6 @@
 // Chỉ những gì Client Component dùng được. Feature này chưa có phần chạy phía
 // server nên không có ngoại lệ nào như `auth` và `account`.
-export { OnboardingGate } from "./components/OnboardingGate";
-export { OnboardingProvider } from "./components/OnboardingProvider";
+export { OnboardingGate } from "./components/views/OnboardingGate";
+export { OnboardingProvider } from "./provider";
 export { useOnboarding } from "./hooks/useOnboarding";
 export { useOnboardingGuard } from "./hooks/useOnboardingGuard";
